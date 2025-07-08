84676c7b4717c02328ce25672cd5118eedf37e3e
lipika1911 <lipikaarya1723@gmail.com>
2025-07-08 16:53:54 +0530

    feat: implement layout structure with navbar, footer, and menu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import {
   createBrowserRouter,
-  RouterProvider,
+  RouterProvider, Outlet
 } from "react-router";
 import Users from './pages/users/Users.tsx';
 import Products from './pages/products/Products.tsx';
 import Home from './pages/home/Home.tsx';
+import Navbar from "./components/navbar/Navbar.tsx";
+import Footer from "./components/footer/Footer.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Login from "./pages/Login/Login.tsx";
 
 function App() {
+
+   const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+   }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        }
+      ]
     },
     {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+      path: "/login",
+      element: <Login />
+    }
   ]);
 
   return (
